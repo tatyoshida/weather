7f1f065e07659bc0bbb09c26839532cc718ceff1
Tatiana Yoshida <tatyoshida@gmail.com>
2023-12-18 00:47:12 -0300

    Added currentTemperature function

--- src/script.js
+++ src/script.js
@@ -1,23 +1,32 @@
+function displayWeather(response) {
+  let currentTemperature = Math.round(response.data.temperature.current);
+  let currentTemperatureElement = document.querySelector("#current-temperature");
+  currentTemperatureElement.innerHTML = `${currentTemperature}`;
+}
 function searchCity(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInputElement.value
+  cityElement.innerHTML = searchInputElement.value;
+  let apiKey = "ofa9b4df40ba3b4e1688atf2bb780ddd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputElement.value}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayWeather);
 }
 
 function formatDate(date) {
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    minutes = `0${minutes}`;    
+    minutes = `0${minutes}`;
   }
-  
+
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  
+
   let day = date.getDay();
-  
+
   let days = [
     "Sunday",
     "Monday",
