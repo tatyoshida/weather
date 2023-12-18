942af46d014444eadab21fd67206de2652a91172
Tatiana Yoshida <tatyoshida@gmail.com>
2023-12-18 00:20:48 -0300

    Added formatDate function

--- src/script.js
+++ src/script.js
@@ -5,5 +5,35 @@ function searchCity(event) {
   cityElement.innerHTML = searchInputElement.value
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;    
+  }
+  
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  
+  let day = date.getDay();
+  
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let formatDay = days[day];
+  return `${formatDay} ${hours}:${minutes}`;
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchCity);
+
+let currentDateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+currentDateElement.innerHTML = formatDate(currentDate);
